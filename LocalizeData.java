e553d0bf5cda29242dfa9210dfe9e392b4a69eed
grgsersan109 <helmipratama07@gmail.com>
2018-12-12 16:10:40 +0700

    This is the first commit after adding localization

---
+++ LocalizeData.java
@@ -0,0 +1,10 @@
+switch (line) {
+case "1" : setEnglish(); run();
+break;
+case "2" : setFrance(); run();
+break;
+case "3" : setChinese(); run();
+break;
+case "4" : setItalian(); run();
+break;
+}
